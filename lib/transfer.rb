d8d4b50ff7ac771837db8635085dfb6738152da2
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 23:37:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    return nil if @status == "complete" or !valid?
+    return nil unles @status == "pending" and valid?
 
     @sender.deposit(-@amount)
     @receiver.deposit(@amount)
