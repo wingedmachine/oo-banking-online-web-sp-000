f4f17b632ef4ba2d9493a39737e7d8ef73baaebc
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 23:23:23 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,9 @@
 class BankAccount
-  attr_reader :name, :balance
+  attr_reader :name, :balance, :status
 
   def initialize(name)
     @name = name
     @balance = 1000
+    @status = "open"
   end
 end
