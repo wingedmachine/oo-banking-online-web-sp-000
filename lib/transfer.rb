a97c73df0354837b1e97d231f4c61a6a95e58d09
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 23:31:53 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,8 +2,8 @@ class Transfer
   attr_reader :sender, :receiver, :status
 
   def initialize(sender, receiver, arg3)
-    @sender = arg1
-    @receiver = arg2
+    @sender = sender
+    @receiver = receiver
     @status = "pending"
   end
 end
